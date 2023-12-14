611c266b54885b6b1890b0d67dfe8db1c9a15991
Shiv Prakash Singh <144921828+shivprakashsinghjii@users.noreply.github.com>
2023-12-14 16:23:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+const input = document.querySelector("input");
+const btn = document.querySelector(".addTask > button");
+
+btn.addEventListener("click", addList);
+input.addEventListener("keyup", (e) => e.keyCode === 13 && addList(e));
+
+function addList(e) {
+  const notCompleted = document.querySelector(".notCompleted");
+  const completed = document.querySelector(".Completed");
+
+  const newLi = document.createElement("li");
+  const checkBtn = createButton('<i class="fa fa-check"></i>');
+  const delBtn = createButton('<i class="fa fa-trash"></i>');
+
+  if (input.value !== "") {
+    newLi.textContent = input.value;
+    input.value = "";
+    notCompleted.appendChild(newLi);
+    appendChildren(newLi, [checkBtn, delBtn]);
+
+    checkBtn.addEventListener("click", () => {
+      notCompleted.removeChild(newLi);
+      completed.appendChild(newLi);
+      checkBtn.style.display = "none";
+    });
+
+    delBtn.addEventListener("click", () => newLi.remove());
+  }
+}
+
+function createButton(innerHtml) {
+  const button = document.createElement("button");
+  button.innerHTML = innerHtml;
+  return button;
+}
+
+function appendChildren(parent, children) {
+  children.forEach((child) => parent.appendChild(child));
+}
